5decb02134762fa63f71b7297b7d3451f349ded3
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-04-07 13:48:10 +0900

    テストに必要なファイルを追加

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController do
   describe 'POST #create' do
-    it 'saves the new article' do
-      expect do
-        post :create, article: FactoryGirl.attributes_for(:article)
-      end.to change(Article, :count).by(1)
+    context 'saves' do
+      it 'the new article' do
+        expect do
+          post :create, article: FG.attributes_for(:article)
+        end.to change(Article, :count).by(1)
+      end
+      it 'redirect to :index' do
+        post :create, article: attributes_for(:article)
+        expect(response).to redirect_to articles_path(assigns([:articles]))
+      end
+    end
+    context 'not saves' do
+      it "redirect to :new" do
+        post :create, article: attributes_for(:article, title: nil)
+        expect(response).to render_template :new
+      end
     end
   end
 end
